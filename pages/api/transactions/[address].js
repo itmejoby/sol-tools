2b9fbf49824223dcd8fe3c9f24db985621e3da12
itmejoby <itmejoby@gmail.com>
2022-10-26 14:03:46 -0400

    remove request limiter

--- pages/api/transactions/[address].js
+++ pages/api/transactions/[address].js
@@ -7,12 +7,9 @@ export default async function handler(req, res) {
   console.log("calling /api/transactions/[address]")
 
   const { address } = req.query;
-
-  // TODO rm later, using to shorten response while testing
-  const beforeUntilParam = "before=ZwbcYp6Een1rUPfGHUz9eUyo69L2AqoyFckha6kTzcftVdQj3URQg1xFeM2xa7vaknaz2iVn5E4T672hCRv7PT6&until=3You6dBegWA9ser83LNdMQuyEtoJyBSj22a5nCtAXZDUXrSDLpKHgrEXDZmjNXkM5Y7KmnShXhX2v9rGa2G2tF9R";
   
   try {
-    const URL = `https://api.helius.xyz/v0/addresses/${address}/transactions?&api-key=${process.env.HELIUS_API_KEY}&${beforeUntilParam}`;
+    const URL = `https://api.helius.xyz/v0/addresses/${address}/transactions?&api-key=${process.env.HELIUS_API_KEY}`;
     const response = await axios.get(URL);
 
     res.status(200).json(response.data)
