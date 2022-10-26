4cdc63ca5af0bb34f94e44ce7abb31e27c1dee26
itmejoby <itmejoby@gmail.com>
2022-10-26 12:51:25 -0400

    handle loading + error visually

--- components/transactions.js
+++ components/transactions.js
@@ -1,14 +1,18 @@
 import getTransactions from '../data/transactions';
 import Table from './table';
+import CircularProgress from '@mui/material/CircularProgress'
+import Alert from '@mui/material/Alert'
 
 export default function Transactions({ address }) {
     const { data, isLoading, isError } = getTransactions(address);
 
-    if (isLoading) return <div>Loading transactions from {address}</div>; // return <Spinner />
-    if (isError) return <div>Error loading transactions from {address}</div>; // return <Error />
+    if (isLoading) return <CircularProgress />;
+    if (isError || data["error"]) return <Alert severity="error">Error loading transactions from {address}</Alert>;
 
     console.log(data)
 
+    // todo handle empty description
+    // todo show more than descriptions
     var descriptions = data.map(function(val, index) {
        return val['description']
     });
